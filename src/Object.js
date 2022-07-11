e6cc316ea3d31f07f325c103a8dd4fa0ee0e8709
prashantwolf <prashantanand630@gmail.com>
2022-07-11 22:15:50 +0530

    first commit

---
+++ src/Object.js
@@ -0,0 +1,59 @@
+let data = [
+    { 
+        id:1,
+        image: "/images/shirt1.jpg",
+        title: "Amazon Logo",
+        type:"shirt",
+        price: 200,
+        totalquantity: 10,
+    },
+    {
+        id:2,
+        image: "/images/pant1.png",
+        title: "React-1 Logo",
+        type:"pant",
+        price: 500,
+        totalquantity: 20,
+
+    },
+    {
+        id:3,
+        image: "/images/pant2.jpg",
+        title: "React-2 Logo",
+        type:"pant",
+        price: 1000,
+        totalquantity: 12,
+
+    },
+    {
+        id:4,
+        image: "/images/pant1.png",
+        title: "React-2 Logo",
+        type:"pant",
+        price: 1000,
+        totalquantity: 3,
+
+    },
+    {
+        id:5,
+        image: "/images/shirt2.jpg",
+        title: "React-2 Logo",
+        type:"shirt",
+        price: 120,
+        totalquantity: 5,
+
+    },
+    {
+        id:6,
+        image: "/images/pant2.jpg",
+        title: "React-2 Logo",
+        type:"pant",
+        price: 600,
+        totalquantity: 6,
+
+    },
+    
+
+
+]
+export default data;
